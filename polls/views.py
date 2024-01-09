fdc80631450dedcc5fb254650d10195f60613d63
Alex Becci <alex.becci@bingopilar.corp>
2024-01-09 12:32:26 -0300

    library added in archive

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import get_object_or_404,render
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 
 """ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date"[:5])
@@ -30,4 +31,20 @@ def results(request,question_id):
     return HttpResponse(response % question_id)
 
 def vote(request,question_id):
-    return HttpResponse("You re voting on question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice.set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,"polls/detail.html",
+            {
+                "question" : question,
+                "error_message": "You didn`t select a choice.",
+                
+            }
+        )
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
